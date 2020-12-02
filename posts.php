2cb100a0b04ebeb94ef37841e114d1fad59a429e
Imirami <74984864+Imirami@users.noreply.github.com>
2020-12-02 10:45:08 -0800

    Create posts.php

---
+++ posts.php
@@ -0,0 +1,10 @@
+<body>
+  <ul id="orders">
+    <li>Person = Imran, Age = 11</li>
+  </ul>
+  <p>Name:</p>
+  <input type="text" id="name">
+  <br>
+  <p>Age</p>
+  <input type="text" id="age">
+</body>
